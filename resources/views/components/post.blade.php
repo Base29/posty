c0676ce0120e71f5342b898c2d4c23d924441bfe
Faisal Hussain <faisal@base29.com>
2021-03-11 14:38:11 +0500

    Created method to view likes recived on overall post

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,3 +1,39 @@
+@props(['post' => $post])
 <div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+    <div class="mb-5 border-b-2 border-gray-400">
+        <div class="mb-4">
+            <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a> <span
+                class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+            <p class="mb-2">{{ $post->body }}</p>
+        </div>
+
+        @can('delete', $post)
+            <form action="{{ route('posts.delete', $post) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="text-blue-500">Delete</button>
+            </form>
+        @endcan
+
+        <div class="flex items-center">
+            @auth
+                @if (!$post->likedBy(auth()->user()))
+                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                        @csrf
+                        <button type="submit" class="text-blue-500">Like</button>
+                    </form>
+                @else
+                    <form action="{{ route('posts.likes', $post) }}" method="POST" class="mr-1">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-blue-500">Unlike</button>
+                    </form>
+                @endif
+
+            @endauth
+
+
+            <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+        </div>
+    </div>
 </div>
