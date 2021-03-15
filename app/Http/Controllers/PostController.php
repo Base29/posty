f60894638805abea76e3f874329988214083276b
Faisal Hussain <faisal@base29.com>
2021-03-15 12:51:20 +0500

    added ray debugging package

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->with(['user', 'likes'])->paginate(10);
+
         return view('posts.index', ['posts' => $posts]);
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex justify-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg">
-        Home
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            @ray('This is home')
+            Home
+        </div>
     </div>
-</div>
 @endsection
